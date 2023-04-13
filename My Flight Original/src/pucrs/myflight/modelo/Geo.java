d8bfa5decd4c9c596c5918df81b2410722045081
otaviofpcunha <otaviofpcunha@gmail.com>
2023-04-13 15:48:31 -0300

    fórmula de haversine

--- My Flight Original/src/pucrs/myflight/modelo/Geo.java
+++ My Flight Original/src/pucrs/myflight/modelo/Geo.java
@@ -16,4 +16,12 @@ public class Geo {
 	public double getLongitude() {
 		return longitude;
 	}
+
+	public static double getDistancia(double latitude1, double latitude2, double longitude1, double longitude2){
+		double  distancia;
+		double raioTerra = 6371;
+		distancia = 2*raioTerra*Math.asin(Math.sqrt(Math.pow(Math.sin ((Math.toRadians(latitude1) - Math.toRadians(latitude2))/2), 2)+ (Math.pow(Math.sin ((longitude1 - longitude2)/2), 2))* Math.cos(Math.toRadians(longitude1)) * Math.cos(Math.toRadians(longitude2))));     
+		return distancia;
+	}
+
 }
